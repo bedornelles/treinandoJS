c60db299ca50cd7078692ce1452e02ce5eafe025
Bernardo Dornelles <bernardo.dornelles22@gmail.com>
2022-06-02 19:10:09 -0300

    atualizando

---
+++ helloWorld.js
@@ -0,0 +1,93 @@
+console.log("Exemplo de aula.");
+var mediaFinal = 0;
+var avaliacaoParcial1 = 1;
+
+console.log ("avaliacao parcial " + avaliacaoParcial1);
+var avaliacaoParcial2 = 2;
+console.log("avaliacao parcial2 " + avaliacaoParcial2);
+
+var ativSemestral = 5;
+console.log("avaliacao semestral " + ativSemestral);
+
+media = avaliacaoParcial1 + avaliacaoParcial2 + ativSemestral;
+
+console.log("Sua média é: " + media);
+
+
+console.log ("Imprima a média aritmética de 3 números")
+var mediaAritmetica = 0;
+
+var primeiroNumero = 3;
+console.log ("primeiro numero " + primeiroNumero);
+
+var segundoNumero = 6;
+console.log ("segundo numero " + segundoNumero);
+
+var terceiroNumero = 9;
+console.log ("terceiro numero " + terceiroNumero);
+
+mediaAritmetica = (primeiroNumero + segundoNumero + terceiroNumero)/3;
+console.log ("A média é: " + mediaAritmetica);
+
+
+console.log ("Simule as notas de um aluno da Ulbra. AP1, AP2, AS e média final");
+var mediaFinal = 0;
+
+var ap1 = 1.5;
+console.log ("avaliação parcial 1:  " + ap1);
+
+var ap2 = 2.5;
+console.log ("avaliação parcial 2:  " + ap2);
+
+var as = 6;
+console.log ("avaliação semestral:  " + as);
+
+mediaFinal = ap1 + ap2 + as;
+console.log ("A média final é:  " + mediaFinal);
+
+
+console.log ("Informe o nome e a idade de uma pessoa e imprima se esta pessoa é maior ou menor de idade");
+var nome = "Bernardo";
+console.log ("O nome é:" + nome);
+
+var idade = 24;
+console.log ("Sua idade é: " + idade);
+
+if (idade >= 18){
+    console.log ("Maior de idade")
+}
+
+else{
+    console.log ("Menor de idade")
+}
+
+console.log ("Informe 3 números e mostre qual é o maior");
+var n1 = 10;
+console.log ("O primeiro número é: " + n1);
+
+var n2 = 22;
+console.log ("O segundo número é: " + n2);
+
+var n3 = 16;
+console.log ("O terceiro número é: " + n3);
+
+if (n1>n2 && n1>n3){
+    console.log ("O maio número é: " + n1)
+}
+else if (n2>n1 && n2>n3){
+    console.log ("O maior número é: "+ n2)
+}
+else if (n3>n1 && n3 >n2){
+    console.log ("O maior número é: " + n3)
+}
+
+
+
+
+
+
+
+
+
+
+
